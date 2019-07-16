84ce0ee51ca7a12d3bd957635483b1d277059e68
Marian Zburlea <marianzburlea@gmail.com>
2019-07-17 00:24:26 +0100

    feat: add section where info for the website should be
    Co-authored-by: Valentin <nicolae9931@yahoo.com>

--- src/component/app/app.component.jsx
+++ src/component/app/app.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { StyledApp } from './app.style';
 import WidgetBot from '@widgetbot/react-embed'
+import WebInfo from '../web-info';
 
 export const CodeTapAcademy = () => {
   return (
     <StyledApp>
+      <WebInfo />
       <WidgetBot
         server="591067148403867650"
         channel="591067149108379692"
---
+++ src/component/web-info/web-info.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const WebInfo = () => {
+  return (
+    <div>
+      <h1>Info about course released, mentoring and much more</h1>
+      <button>Log in</button>
+    </div>
+  )
+}
+
+export default WebInfo
