cf2267c823e06f71d6507b936139a1b5e1778d29
Marian Zburlea <marianzburlea@gmail.com>
2019-07-20 03:31:19 +0100

    feat: add Roboto font to the webpage as the default one this fixes #59

--- src/component/web-info/web-info.style.js
+++ src/component/web-info/web-info.style.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 import { Router } from '@reach/router'
 
 export const StyledRouter = styled(Router)`
-  padding: 2rem 1rem 0;
+  padding: 3.5rem 1rem 0;
 `
 
 export const StyledWebInfo = styled.div`
