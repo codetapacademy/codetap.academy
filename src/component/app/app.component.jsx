cf8a1d11bcce10bd6fdf09e573e09658883d0418
Marian Zburlea <marianzburlea@gmail.com>
2019-07-22 20:07:56 +0100

    feat: re-enable the chat bot this fixes #76

--- src/component/app/app.component.jsx
+++ src/component/app/app.component.jsx
@@ -1,25 +1,27 @@
 import React from 'react'
-import { StyledApp } from './app.style';
+import { StyledApp, StyledWidgetWrapper } from './app.style';
 import WidgetBot from '@widgetbot/react-embed'
 import WebInfo from '../web-info';
 
 export const CodeTapAcademy = () => {
-  const showWidgetBot = false
+  const showWidgetBot = true
   return (
     <StyledApp showWidgetBot={showWidgetBot}>
       <WebInfo />
-      {showWidgetBot && <WidgetBot
-        server="591067148403867650"
-        channel="591067149108379692"
-        shard="https://disweb.deploys.io"
-        location={[
-          'bottom',
-          'right'
-        ]}
-        color="#d52027"
-        width="100%"
-        height="100%"
-        />}
+      <StyledWidgetWrapper>
+        {showWidgetBot && <WidgetBot
+          server="591067148403867650"
+          channel="591067149108379692"
+          shard="https://disweb.deploys.io"
+          location={[
+            'bottom',
+            'right'
+          ]}
+          color="#d52027"
+          width="100%"
+          height="100%"
+          />}
+      </StyledWidgetWrapper>
     </StyledApp>
   )
 }
