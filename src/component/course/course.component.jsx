9c5e39aaf0bcedd9a4e2725b35bd546719d309d5
Marian Zburlea <marianzburlea@gmail.com>
2019-07-27 19:34:43 +0100

    refactor: course data comes in just for the course id needed

--- src/component/course/course.component.jsx
+++ src/component/course/course.component.jsx
@@ -5,55 +5,64 @@ import PanelTitle from '../panel-title';
 import SectionPanel from '../section-panel/section-panel.component';
 
 const Course = ({ courseId }) => {
-  const { courseList } = WebInfoState()
+  // const { courseList } = WebInfoState()
   const [ lectureBySectionIdList, setLectureBySectioIdList ] = useState({})
   const [ course, setCourse ] = useState({})
 
   useEffect(() => {
-    console.log('useEffect')
-    // get the course title and descripton
-    const course = courseList.filter(({ id }) => id === courseId)[0]
-    if (course && courseList.length) {
-      setCourse(course)
-    }
-
-    // get a list of lectures filtered by the id of the course
-    // and then filter the lectures by the section id in an object
-    // that has the id of the section as a key and the value of it
-    // as the lecture (title, desciption and id)
-    db
-      .collection('lecture')
-      .where('course.id', '==', courseId)
-      .get()
-      .then(snapList => {
-        const objectList = {}
-        snapList.docs.forEach(doc => {
+    (async () => {
+      // I want to get the course info
+      const course = await db
+        .collection('course')
+        .doc(courseId)
+        .get()
+      
+      console.log('useEffect', courseId, setCourse(course.data()))
+      // I want to get all the lectures based on the courseId
+      try {
+        const lectureSnapshotList = await db
+          .collection('lecture')
+          .where('course.id', '==', courseId)
+          .get()
+        const lectureKeyList = {}
+        lectureSnapshotList.docs.forEach(doc => {
           const lectureId = doc.id;
           const lectureContent = doc.data()
-          if (objectList.hasOwnProperty(lectureContent.section.id)) {
-            objectList[lectureContent.section.id] = [...objectList[lectureContent.section.id], {
+          if (lectureKeyList.hasOwnProperty(lectureContent.section.id)) {
+            lectureKeyList[lectureContent.section.id] = [...lectureKeyList[lectureContent.section.id], {
               id: lectureId,
               ...lectureContent
             }]
           }
           else {
-            objectList[lectureContent.section.id] = [{
+            lectureKeyList[lectureContent.section.id] = [{
               id: lectureId,
               ...lectureContent
             }]
           }
         })
+        console.log(lectureKeyList)
+      } catch (e) {
+        console.error('Get lectureSnapshotList failed', e)
+      }
+    })()
 
-        console.log('setLectureBySectioIdList')
-        setLectureBySectioIdList(objectList)
-      })
+    // get a list of lectures filtered by the id of the course
+    // and then filter the lectures by the section id in an object
+    // that has the id of the section as a key and the value of it
+    // as the lecture (title, desciption and id)
+    //   .then(snapList => {
+
+    //     console.log('setLectureBySectioIdList')
+    //     setLectureBySectioIdList(objectList)
+    //   })
     // return lectureUnsubscribe()
-  }, [courseList])
+  }, [])
 
   return (
     <div>
       <PanelTitle>{course.title}</PanelTitle>
-      {console.log(courseList)}
+      {/* {console.log(courseList)} */}
       <p>{course.description}</p>
       {course && course.id && <SectionPanel course={course} />}
     </div>
