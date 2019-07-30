41e5198067cd7fd49003a5c22179e0c16e7fe161
QuintyHH <razmocanu@yahoo.com>
2019-07-30 21:01:19 +0300

    images can be added to lectures this resolves #101

--- src/component/lecture/lecture.component.jsx
+++ src/component/lecture/lecture.component.jsx
@@ -1,26 +1,41 @@
-import React, { useEffect, useState } from 'react'
-import { db } from '../data/firebase'
-import { Link } from '@reach/router';
+import React, { useEffect, useState } from "react";
+import { db } from "../data/firebase";
+import { Link } from "@reach/router";
+import firebase from "firebase";
+import FileUploader from "react-firebase-file-uploader";
 
 const Lecture = ({ lectureId }) => {
-  const [ lecture, setLecture ] = useState()
+  const [lecture, setLecture] = useState();
+
   useEffect(() => {
     (async () => {
       const lectureSnapshot = await db
-        .collection('lecture')
+        .collection("lecture")
         .doc(lectureId)
-        .get()
+        .get();
 
-      setLecture(lectureSnapshot.data())
-    })()
-  }, [lectureId])
+      setLecture(lectureSnapshot.data());
+    })();
+  }, [lectureId]);
 
   return (
     <div>
       <h1>Lecture {lectureId}</h1>
-      {lecture && lecture.course && <p>Back to <Link to={`/course/${lecture.course.id}`}>{lecture.course.title}</Link></p> }
+      {lecture && lecture.course && (
+        <p>
+          Back to{" "}
+          <Link to={`/course/${lecture.course.id}`}>
+            {lecture.course.title}
+          </Link>
+        </p>
+      )}
+      <FileUploader
+        accept="image/*"
+        name="image"
+        storageRef={firebase.storage().ref("lecture-pictures")}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Lecture
+export default Lecture;
