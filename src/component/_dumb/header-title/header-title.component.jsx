0d5253d74ed7c255b43f5093db54b7c2766c797a
Marian Zburlea <marianzburlea@gmail.com>
2019-08-03 19:50:42 +0100

    feat: Implement a logo on the top menu for CodeTap Academy with a slogan

--- src/component/_dumb/header-title/header-title.component.jsx
+++ src/component/_dumb/header-title/header-title.component.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { DynamicStyledTitle } from './header-title.style';
 
-const HeaderTitle = ({ text = 'Change Me', tag = 'h1' }) => {
-  return <DynamicStyledTitle tag={tag}>{text}</DynamicStyledTitle>
+const HeaderTitle = ({ text = 'Change Me', tag = 'h1', title = '' }) => {
+  return <DynamicStyledTitle tag={tag} title={title}>{text}</DynamicStyledTitle>
 }
 
 export default HeaderTitle
