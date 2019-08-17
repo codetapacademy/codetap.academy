59ca3d63147ada865e633b182d0e611c3557294f
Marian Zburlea <marianzburlea@gmail.com>
2019-08-17 21:40:14 +0100

    feat: generate YouTube preview using the default image from YouTube thumbnail if no image is currently uploaded for the current lecture this fixes #146

--- src/component/lecture-info/lecture-info.component.jsx
+++ src/component/lecture-info/lecture-info.component.jsx
@@ -1,23 +1,28 @@
-import React from 'react'
+import React from 'react';
 import HeaderTitle from '../_dumb/header-title';
 import ResponsiveThumbnail from '../_dumb/responsive-thumbnail';
-import { StyledLectureInfo } from './lecture-info.style'
+import { StyledLectureInfo } from './lecture-info.style';
 
-const LectureInfo = ({ title, id, description, imagePath }) => {
+const LectureInfo = ({ title, id, description, imagePath, youtubeVideoId }) => {
+  console.log(youtubeVideoId, imagePath);
+  // http://img.youtube.com/vi/A-iTEtt6SN8/0.jpg
   const headerTitlePropList = {
     text: title,
     tag: 'h4',
-    fontSize: '13px',
-  }
+    fontSize: '13px'
+  };
+
+  const getImagePath = () =>
+    imagePath ? imagePath : `http://img.youtube.com/vi/${youtubeVideoId}/0.jpg`;
 
   return (
     <StyledLectureInfo>
       <div>
-        <ResponsiveThumbnail imagePath={imagePath} />
+        <ResponsiveThumbnail imagePath={getImagePath()} />
       </div>
       <HeaderTitle {...headerTitlePropList} />
     </StyledLectureInfo>
-  )
-}
+  );
+};
 
-export default LectureInfo
+export default LectureInfo;
