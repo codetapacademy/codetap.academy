c072dadc5d0420d122ca0b16ecb26be6cf8712c2
psyhedeliq <filipcornelcristian@gmail.com>
2019-08-17 01:41:48 +0100

    feat: log in or out to dashboard this fixes #137

--- src/component/top-menu/top-menu.component.jsx
+++ src/component/top-menu/top-menu.component.jsx
@@ -1,29 +1,33 @@
-import React from 'react'
-import { StyledTopMenu, StyledLink, StyledButton } from './top-menu.style'
+import React from 'react';
+import { StyledTopMenu, StyledLink, StyledButton } from './top-menu.style';
 import { WebInfoState } from '../web-info/web-info.context';
 import { auth, GitHubProvider, db } from '../data/firebase';
 import Avatar from '../avatar';
+import { navigate } from '@reach/router';
 
 const TopMenu = () => {
-  const { toggleChat, updateToggleChat, user, updateUser } = WebInfoState()
+  const { toggleChat, updateToggleChat, user, updateUser } = WebInfoState();
 
   const handleToggleChat = () => {
     updateToggleChat({ type: 'TOGGLE_CHAT' });
-  }
+  };
 
   const handleLogInAndOut = () => {
     if (user) {
       auth.signOut();
+      navigate('/');
       updateUser({
         type: 'USER_AUTHENTICATE',
         user: null
-      })
-    }
-    else {
+      });
+    } else {
       auth
         .signInWithPopup(GitHubProvider)
-        .then(({ user: { uid, displayName, photoURL, email }}) => {
-          db.collection('user').doc(uid).set({ displayName, photoURL, email }, { merge: true })
+        .then(({ user: { uid, displayName, photoURL, email } }) => {
+          db.collection('user')
+            .doc(uid)
+            .set({ displayName, photoURL, email }, { merge: true });
+          navigate('/dashboard');
           updateUser({
             type: 'USER_AUTHENTICATE',
             user: {
@@ -31,29 +35,27 @@ const TopMenu = () => {
               displayName,
               photoURL
             }
-          })
+          });
         })
         .catch(error => {
           // console.error(error)
-        })
+        });
     }
-  }
+  };
 
-  const getLogInOutLabel = () => user ? 'Ba Bye' : 'Let me in!'
+  const getLogInOutLabel = () => (user ? 'Ba Bye' : 'Let me in!');
 
   return (
     <StyledTopMenu>
       <StyledLink to="/">Home</StyledLink>
       <StyledLink to="/dashboard">Dashboard</StyledLink>
       <Avatar user={user} />
-      <button onClick={handleLogInAndOut}>
-        {getLogInOutLabel()}
-      </button>
-      <StyledButton onClick={handleToggleChat}>{
-        toggleChat ? 'Invizi Chat' : 'Gimme chat now!'
-      }</StyledButton>
+      <button onClick={handleLogInAndOut}>{getLogInOutLabel()}</button>
+      <StyledButton onClick={handleToggleChat}>
+        {toggleChat ? 'Invizi Chat' : 'Gimme chat now!'}
+      </StyledButton>
     </StyledTopMenu>
-  )
-}
+  );
+};
 
-export default TopMenu
+export default TopMenu;
