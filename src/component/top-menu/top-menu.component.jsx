e2c830f90005c730809477007bdc3cced7685384
psyhedeliq <filipcornelcristian@gmail.com>
2019-08-17 02:11:47 +0100

    fix: add navigate from reach/router

--- src/component/top-menu/top-menu.component.jsx
+++ src/component/top-menu/top-menu.component.jsx
@@ -5,6 +5,7 @@ import { auth, GitHubProvider, db } from '../data/firebase';
 import Avatar from '../avatar';
 import Logo from '../_dumb/logo/logo.component';
 import HeaderTitle from '../_dumb/header-title';
+import { navigate } from '@reach/router';
 
 const TopMenu = () => {
   const { toggleChat, updateToggleChat, user, updateUser } = WebInfoState();
