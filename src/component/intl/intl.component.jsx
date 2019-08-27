ddc47210bfe627b5146634c05fd38f36e1e4a89e
Robert Alexandru <romaniarobertverde@yahoo.com>
2019-08-27 16:18:12 +0100

    Creating the intl component and verifying if is working

---
+++ src/component/intl/intl.component.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Intl = () => {
+  return <h3>It's working</h3>
+}
+
+export default Intl
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './index.css'
 import { ThemeProvider } from 'styled-components'
 import theme from './component/theme'
 import { WebInfoProvider } from './component/web-info/web-info.context';
+import Intl from './component/intl/intl.component'
 
 const tagId = '#react-will-render-here-and-it-is-really-awesome'
 
@@ -12,6 +13,7 @@ render(
   <ThemeProvider theme={theme}>
     <WebInfoProvider>
         <CodeTapAcademy />
+        <Intl />
     </WebInfoProvider>
   </ThemeProvider>,
   document.querySelector(tagId)
