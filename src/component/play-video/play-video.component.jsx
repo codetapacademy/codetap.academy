e8399188101eae4add9a0b9616206989b1fc8463
Marian Zburlea <marianzburlea@gmail.com>
2019-09-02 04:54:09 +0100

    feat: enable video to play in a playlist on the video page

--- src/component/play-video/play-video.component.jsx
+++ src/component/play-video/play-video.component.jsx
@@ -1,13 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import { StyledVideo, StyledVideoOverlay } from './play-video.style';
 import CommentSection from '../comment-section';
+import { db } from '../data/firebase';
 
 const PlayVideo = ({ youtubeVideoId }) => {
+  const [video, setVideo] = useState('')
+  const lectureCollection = db.collection('lecture')
   useEffect(() => {
     (async () => {
-      console.log(youtubeVideoId)
+      const lectureSnap = await lectureCollection
+        .where('youtubeVideoId', '==', youtubeVideoId)
+        .get()
+      let youtubePlaylistId, order = 0
+      lectureSnap.forEach(doc => {
+        const data = doc.data()
+        youtubePlaylistId = data.course.youtubePlaylistId
+      })
+
+      const lectureListSnap = await lectureCollection
+        .where('course.youtubePlaylistId', '==', youtubePlaylistId)
+        // .orderBy('order', 'asc')
+        .get()
+      lectureListSnap.docs.forEach((doc, key) => {
+        const data = doc.data()
+        if (data.youtubeVideoId === youtubeVideoId) {
+          order = data.order ? data.order : key
+        }
+      })
+      setVideo({ order, youtubePlaylistId })
     })()
-  }, [youtubeVideoId])
+  }, [youtubeVideoId, lectureCollection])
   const handleRightClick = e => {
     e.preventDefault();
     return;
@@ -18,7 +40,7 @@ const PlayVideo = ({ youtubeVideoId }) => {
         <iframe
           width="100%"
           height="100%"
-          src={`https://www.youtube.com/embed/${youtubeVideoId}`}
+          src={`https://www.youtube.com/embed/videoseries?list=${video.youtubePlaylistId}&index=${video.order}&modestbranding=1&rel=0&showinfo=1&loop=0&fs=0&hl=en&enablejsapi=1`}
           frameBorder="0"
           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
