d40aed1d6df7992fc398fc14eafa8ee38db0d83b
Marian Zburlea <marianzburlea@gmail.com>
2019-10-12 12:13:03 +0100

    code clean-up

--- src/component/top-menu/top-menu.style.js
+++ src/component/top-menu/top-menu.style.js
@@ -1,41 +1,4 @@
 import styled from 'styled-components'
-import { Link } from '@reach/router'
-
-export const StyledLogoWrapper = styled.span`
-  display: flex;
-  align-items: center;
-`
-
-export const StyledButton = styled.button`
-  background-color: rgba(114, 137, 218, 0.4);
-  border-radius: 14px;
-  /* border-radius: ${({ theme }) => theme.layout.borderRadius}; */
-  color: ${({ theme }) => theme.layout.text};
-  border: 1px solid ${({ theme }) => theme.layout.borderColor};
-  font-weight: bold;
-  height: 28px;
-  padding: 0 1rem;
-`
-
-export const StyledLink = styled(Link)`
-  text-decoration: none;
-  padding: 0.5rem 1rem;
-  color: white;
-  transition: 0.25s;
-
-  &:first-child {
-    flex-grow: 1;
-  }
-
-  &:not(:first-child) {
-    margin-left: 20px;
-  }
-
-  &:hover {
-    color: #c00;
-    background-color: white;
-  }
-`
 
 export const StyledTopMenu = styled.div`
   height: 64px;
