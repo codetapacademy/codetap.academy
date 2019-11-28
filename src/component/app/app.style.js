5387ff1816cb2ada677b6407917361d8626cc3e3
Marian Zburlea <marianzburlea@gmail.com>
2019-11-28 04:49:55 +0000

    update: manage user admin section

--- src/component/app/app.style.js
+++ src/component/app/app.style.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 export const StyledApp = styled.div`
   height: 100%;
   display: grid;
+  grid-template-rows: auto 1fr;
   background-color: ${({ theme }) => theme.layout.background};
   color: ${({ theme }) => theme.layout.text};
 `
--- src/component/user-item/user-item.component.jsx
+++ src/component/user-item/user-item.component.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Avatar from '../avatar'
 
 const UserItem = ({ user }) => {
-  const { displayName, photoUrl, email, id, subscription: {
+  const { displayName, photoUrl, email, id, accepted, subscription: {
     next_billing_at,
     plan_id
   } = {} } = user
@@ -14,7 +14,7 @@ const UserItem = ({ user }) => {
       <Avatar user={user} />
       <div>{email}</div>
       <div>{plan_id}</div>
-      <div>{plan_id}</div>
+      <div>{accepted ? 'Accepted' : 'Not yet'}</div>
       <div>{getNiceDate(next_billing_at)}</div>
       <div id={id}><a href={`#${id}`} title={id}>#{id.slice(0, 6)}</a></div>
     </>
