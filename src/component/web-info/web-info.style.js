ec846cb2002bf702950597b9d7d60ab855a4cac7
Marian Zburlea <marianzburlea@gmail.com>
2020-01-03 03:00:59 +0000

    update: refactor top menu using position fixed

--- src/component/web-info/web-info.style.js
+++ src/component/web-info/web-info.style.js
@@ -2,15 +2,13 @@ import styled from 'styled-components'
 import { Router } from '@reach/router'
 
 export const StyledRouter = styled(Router)`
-  /* margin: 4.5rem 1rem 0; */
   background-color: ${({ theme }) => theme.menu.backgroundSelected};
-  /* min-height: 100%; */
-  margin: ${({ theme }) => theme.layout.bigMargin};
   padding: ${({ theme }) => theme.layout.bigPadding};
   border-radius: ${({ theme }) => theme.layout.borderRadius};
-  `
+`
 
 export const StyledWebInfo = styled.div`
-  height: 100%;
-  overflow-y: auto;
+  min-height: 100%;
+  padding: ${({ theme }) => theme.layout.bigPadding};
+  padding-top: 84px;
 `
