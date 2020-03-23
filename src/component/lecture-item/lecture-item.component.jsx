21adcfec5e7108ae1729c01b408c2c05e53e6092
Marian Zburlea <marianzburlea@gmail.com>
2020-03-23 22:46:08 +0000

    feat: display published and level in the course listing

--- src/component/lecture-item/lecture-item.component.jsx
+++ src/component/lecture-item/lecture-item.component.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import { StyledLectureItem, StyledLink } from './lecture-item.style';
 import Pill from '../_dumb/pill'
 
-const LectureItem = ({ id, title, description, remove, sectionId, duration }) => {
+const LectureItem = ({ id, title, description, remove, sectionId, duration, levelRequired, published, ...rest }) => {
   const handleRemove = () => {
     remove(id, sectionId)
   }
+  console.log(rest)
   return (
     <StyledLectureItem>
       <StyledLink to={`/manage/lecture/${id}`} title={description}>{title}</StyledLink>
+      <Pill label="Level" value={levelRequired} />
+      <Pill label="Published" value={published ? 'Yes' : 'No'} />
       <Pill label="Time" value={duration} />
       <button onClick={handleRemove}>&times;</button>
     </StyledLectureItem>
