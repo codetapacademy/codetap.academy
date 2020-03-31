859cf9934f540d6c1483369b2ac969671c9cd579
Marian Zburlea <marianzburlea@gmail.com>
2020-04-01 00:36:58 +0100

    feat: create a copy button for title and description this closes #357
    Co-authored-by: Filip Cornel-Cristian <filipcornelcristian@gmail.com>

--- src/component/lecture-item/lecture-item.component.jsx
+++ src/component/lecture-item/lecture-item.component.jsx
@@ -1,15 +1,45 @@
-import React from 'react'
-import { StyledLectureItem, StyledLink } from './lecture-item.style';
+import React, { useRef } from 'react'
+import { StyledLectureItem, StyledLink, StyledHiddenTextarea, StyledCopyButton } from './lecture-item.style';
 import Pill from '../_dumb/pill'
 
 const LectureItem = ({ id, title, description, remove, sectionId, duration, levelRequired, published, ...rest }) => {
+  const titleContent = useRef()
+  const descriptionContent = useRef()
+
   const handleRemove = () => {
     remove(id, sectionId)
   }
-  console.log(rest)
+
+  const copyText = e => {
+    const { name } = e.target
+    const copyReferenceHolder = {
+      title: titleContent,
+      description: descriptionContent
+    }
+
+    if (copyReferenceHolder.hasOwnProperty(name)) {
+      copyReferenceHolder[name].current.select()
+      document.execCommand('copy')
+    }
+  }
+
   return (
     <StyledLectureItem>
       <StyledLink to={`/manage/lecture/${id}`} title={description}>{title}</StyledLink>
+      <StyledCopyButton name="title" onClick={copyText}>CT</StyledCopyButton>
+      <StyledCopyButton name="description" onClick={copyText}>CD</StyledCopyButton>
+
+      <StyledHiddenTextarea
+        ref={titleContent}
+        tabIndex="-1"
+        defaultValue={title}
+      />
+      <StyledHiddenTextarea
+        ref={descriptionContent}
+        tabIndex="-1"
+        defaultValue={description}
+      />
+
       <Pill label="Level" value={levelRequired} />
       <Pill label="Published" value={published ? 'Yes' : 'No'} />
       <Pill label="Time" value={duration} />
